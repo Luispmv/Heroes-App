ba4ada5ab52352b2ca828424821fb3a4e92f4758
Luispmv <luispmv07@hotmail.com>
2024-09-09 00:18:42 -0600

    Agregando funcionalidad de logout a nuestra aplicacion, esta funcionalidad se encuentra en PublisherActivity.kt

--- app/src/main/java/com/example/heroesapp/activities/PublisherActivity.kt
+++ app/src/main/java/com/example/heroesapp/activities/PublisherActivity.kt
@@ -1,21 +1,34 @@
 package com.example.heroesapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.heroesapp.MainActivity
 import com.example.heroesapp.R
 
 class PublisherActivity : AppCompatActivity() {
+    lateinit var logoutBtn : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.publisher_activity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val sharedPreferences = getSharedPreferences("myPrefs", MODE_PRIVATE)
+        logoutBtn = findViewById(R.id.logoutBtn)
+        logoutBtn.setOnClickListener{
+            Log.i("LOGOUT","CERRANDO SESION")
+            val editor = sharedPreferences.edit()
+            editor.remove("isLogged")
+            editor.apply()
+
+            // Para navegar entre activities usamos intent
+            val intent = Intent(this@PublisherActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
