be05ecf78157a16c6ddf0dfd4b3702dc1b70ac46
Luispmv <luispmv07@hotmail.com>
2024-09-13 10:55:57 -0600

    Añadiendo navegacion entre paginas en Hero Detail

--- app/src/main/java/com/example/heroesapp/activities/HeroDetailActivity.kt
+++ app/src/main/java/com/example/heroesapp/activities/HeroDetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example.heroesapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,11 +10,20 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.heroesapp.R
 
 class HeroDetailActivity : AppCompatActivity() {
+
+    lateinit var backbtn : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_hero_detail)
 
-        //Aqui va el codigo
+
+        //Regresando al activity anterior
+        backbtn = findViewById(R.id.back_list)
+        backbtn.setOnClickListener{
+            val intent = Intent(this@HeroDetailActivity, PublisherActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
